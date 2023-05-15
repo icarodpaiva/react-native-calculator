45cfc89b3bfe1a6e781eb696ec3866d3d8e87a06
Icaro Paiva <icaro.paiva@acct.global>
2023-05-15 20:59:24 -0300

    calculator finished

--- App.tsx
+++ App.tsx
@@ -1,25 +1,73 @@
 import React, { useState } from "react"
 import { View, StyleSheet } from "react-native"
 
-import { Display } from "./src/components/Display"
+import { Display } from "./src/components/Display/Display"
 import { Button } from "./src/components/Button/Button"
 
-export const App = () => {
-  const [displayValue, setDisplayValue] = useState("0")
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null as null | string,
+  values: [0, 0],
+  current: 0 as 0 | 1
+}
 
-  const addDigit = (label: string) => {
-    setDisplayValue(prev => (prev === "0" ? label : `${prev}${label}`))
+export const App = () => {
+  const [state, setState] = useState({ ...initialState })
+
+  const addDigit = (digit: string) => {
+    const clearDisplay = state.displayValue === "0" || state.clearDisplay
+
+    if (digit === "." && !clearDisplay && state.displayValue.includes(".")) {
+      return
+    }
+
+    const currentValue = clearDisplay ? "" : state.displayValue
+    const displayValue = currentValue + digit
+
+    setState({ ...state, displayValue, clearDisplay: false })
+
+    if (digit !== ".") {
+      const newValue = parseFloat(displayValue)
+      const values = [...state.values]
+      values[state.current] = newValue
+
+      setState({ ...state, displayValue, clearDisplay: false, values })
+    }
   }
 
-  const setOperation = (opeartion: string) => {}
-
   const clearMemory = () => {
-    setDisplayValue("0")
+    setState({ ...initialState })
+  }
+
+  const setOperation = (operation: string) => {
+    if (state.current === 0) {
+      setState({ ...state, operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === "="
+      const values = [...state.values]
+
+      try {
+        values[0] = eval(`${values[0]} ${state.operation} ${values[1]}`)
+      } catch {
+        values[0] = state.values[0]
+      }
+
+      values[1] = 0
+
+      setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   return (
     <View style={style.container}>
-      <Display displayValue={displayValue} />
+      <Display displayValue={state.displayValue} />
 
       <View style={style.buttons}>
         <Button label="AC" onPress={clearMemory} buttonTriple />
